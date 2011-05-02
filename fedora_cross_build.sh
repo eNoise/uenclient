04e52b538fe81c363743b540044e9b81690e820c
DEgx <deg@uruchie.org>
2011-05-02 15:31:53 +0300

    Some external libs

--- fedora_cross_build.sh
+++ fedora_cross_build.sh
@@ -9,7 +9,7 @@ mingw32-make -j4
 mingw32-make install
 cd uenclient
 i686-pc-mingw32-strip uenclient.exe
-cp /usr/i686-pc-mingw32/sys-root/mingw/bin/{QtCore4.dll,QtGui4.dll,QtNetwork4.dll,QtScript4.dll,libstdc++-6.dll,libgcc_s_sjlj-1.dll,zlib1.dll,libpng14-14.dll} ./
+cp /usr/i686-pc-mingw32/sys-root/mingw/bin/{QtCore4.dll,QtGui4.dll,QtNetwork4.dll,QtScript4.dll,libstdc++-6.dll,libgcc_s_sjlj-1.dll,zlib1.dll,libpng14-14.dll,QtSvg4.dll,libjpeg-7.dll,QtXml4.dll,libtiff-3.dll} ./
 i686-pc-mingw32-strip libstdc++-6.dll
 cd ../
 zip -r uenclient-testbuild.zip uenclient/
