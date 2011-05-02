b343af50f8da22b0683914b3cc778894dfc551c0
DEgx <deg@uruchie.org>
2011-05-02 15:44:11 +0300

    debug script fix

--- fedora_debug_build.sh
+++ fedora_debug_build.sh
@@ -3,4 +3,5 @@ cd win32build
 mingw32-cmake -DCMAKE_BUILD_TYPE=Debug -DLOCAL_GLOOX=ON -DCMAKE_INSTALL_PREFIX=`pwd`/uenclient ../
 mingw32-make -j4
 mingw32-make install
-cp /usr/i686-pc-mingw32/sys-root/mingw/bin/{QtCored4.dll,QtGuid4.dll,QtNetworkd4.dll,QtScriptd4.dll} ./
+cd uenclient
+cp /usr/i686-pc-mingw32/sys-root/mingw/bin/{QtCored4.dll,QtGuid4.dll,QtNetworkd4.dll,QtScriptd4.dll,QtSvgd4.dll,QtXmld4.dll} ./
